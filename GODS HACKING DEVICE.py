b77c699e6e3c85a5d3f2c182916f7c21cff1ddb4
Akshay Arjun <74445078+AkshayArjun@users.noreply.github.com>
2020-12-03 16:51:01 +0530

    Add files via upload

---
+++ GODS HACKING DEVICE.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import time
+# Python program to print 
+# colored text and background 
+def prRed(skk): print("\033[91m {}\033[00m" .format(skk)) 
+def prGreen(skk): print("\033[92m {}\033[00m" .format(skk)) 
+def prYellow(skk): print("\033[93m {}\033[00m" .format(skk)) 
+def prLightPurple(skk): print("\033[94m {}\033[00m" .format(skk)) 
+def prPurple(skk): print("\033[95m {}\033[00m" .format(skk)) 
+def prCyan(skk): print("\033[96m {}\033[00m" .format(skk)) 
+def prLightGray(skk): print("\033[97m {}\033[00m" .format(skk)) 
+def prBlack(skk): print("\033[98m {}\033[00m" .format(skk)) 
+s = input("Enter your user name : ")
+try:
+    float(s)
+    prGreen("Sorry, Enter Correct User Name ")
+except ValueError:
+    prGreen("Welcome Boss! I Have Been Waiting For You !")
+checky = input("Hey Boss! Do You Agree To Use this Tool For educational purpose Only! I know You won't Use for Educational purpose But I Need To ask it Yes or No: ")
+if checky == ("yes"):
+    prGreen("Thanks For Using It For educational Purpose! ")
+else :
+    prRed("I Knew That You Won't Use It For Educational Purpose! Anyway For Being Honest I'm Allowing You To Use This Tool")
+name = input("Enter The Organization Name You Want To Hack or Enter Their Website : ")
+prRed("         This Tool Is created BY      ")
+prRed("✩░▒▓▆▅▃▂▁𝐀𝐤𝐬𝐡𝐚𝐲 𝐀𝐫𝐣𝐮𝐧▁▂▃▅▆▓▒░✩")
+prYellow("Instgarm.com/_thearjun_ & Instagram.com/python_code_camp")
+prGreen("Checking " +  name )
+time.sleep(3)
+prGreen("Accessing " +  name + ".............")
+time.sleep(3)
+prGreen("Hacking Started on " +  name )
+prGreen("Connected TO Their Database")
+time.sleep(3)
+prGreen("Installing Malware In their Servers")
+time.sleep(3)
+prGreen("Installion Failed!")
+time.sleep(3)
+prGreen("Re-trying attack on "  +  name)
+time.sleep(5)
+prGreen("Malware injected successfully")
+prGreen("Hacking ............../10%")
+prGreen("Hacking ............../20%")
+time.sleep(3)
+prGreen("Hacking ............../25%")
+prGreen("Hacking ............../30%")
+time.sleep(5)
+prGreen("Hacking ............../34%")
+prGreen("Hacking ............../40%")
+time.sleep(3)
+prGreen("Hacking ............../45%")
+prGreen("Hacking ............../49%")
+time.sleep(5)
+prGreen("Hacking ............../50%")
+prGreen("Hacking ............../55%")
+time.sleep(3)
+prGreen("Hacking ............../60%")
+prGreen("Hacking ............../65%")
+time.sleep(3)
+prGreen("Hacking ............../69%")
+prGreen("Hacking ............../70%")
+time.sleep(3)
+prGreen("Hacking ............../75%")
+prGreen("Hacking ............../80%")
+prGreen("Hacking ............../90%")
+time.sleep(5)
+prGreen("Hacking ............../100%")
+time.sleep(3)
+prGreen("Hack Succesfull")
+time.sleep(3)
+prGreen("Deleting Our Traces From Their Servers")
+time.sleep(3)
+prGreen("Delete Successfull")
+time.sleep(2)
+inp = input("Do You Want To Copy All Data From " + name + "! Yes or No : ")
+if inp == ("yes"):
+    prGreen("Copying Files")
+    time.sleep(3)
+    prGreen("Copy Successfull ")
+else :
+    prGreen("Ok Bye Boss! We have Successfully Hacked " + name + ".You can Use Me Anytime you want i Will Be Waiting For You!" )
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
